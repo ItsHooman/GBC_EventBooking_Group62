2a4f5e918894fcb3360dd74f72b6a3b7703d72d6
AureliusJ <141293290+AureliusJ@users.noreply.github.com>
2024-11-08 14:05:40 -0500

    hopefully it works

--- ApprovalService/build.gradle.kts
+++ ApprovalService/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.3.5"
+	id("org.springframework.boot") version "3.3.5" // Make sure this Spring Boot version is compatible with Java 21
 	id("io.spring.dependency-management") version "1.1.6"
 }
 
@@ -9,7 +9,7 @@ version = "0.0.1-SNAPSHOT"
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
+		languageVersion.set(JavaLanguageVersion.of(21))
 	}
 }
 
@@ -20,18 +20,18 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-	implementation ("org.springframework.cloud:spring-cloud-starter-openfeign")
 }
 
 dependencyManagement {
 	imports {
 		mavenBom("org.springframework.cloud:spring-cloud-dependencies:2021.0.4")
 	}
+}
 
-	tasks.withType<Test> {
-		useJUnitPlatform()
-	}
+tasks.withType<Test> {
+	useJUnitPlatform()
 }
